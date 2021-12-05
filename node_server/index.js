162e934227b34e2bfe78e2c50f4cda472733fcbd
Dhyana Swain <dhyana@Dhyanas-MacBook-Air.local>
2021-12-06 03:18:48 +0530

    ==

--- node_server/index.js
+++ node_server/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
 // App
 const app = express()
+const port = '4000';
 
 // cors
 app.use(cors())
@@ -14,9 +18,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes/index.routes'))
 
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
+
 // First route
 app.get('/', (req, res) => {
     res.json({ message: 'Hello world' })
 })
 // Starting server
-app.listen('4000')
+app.listen(port, () => {
+    console.log(`Server listening on the port::::::${port}`);
+});
